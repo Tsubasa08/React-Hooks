22b882855bc2610b9fad7163cbdcea5d4ffb4de3
Tsubasa08 <46633705+Tsubasa08@users.noreply.github.com>
2023-01-02 23:49:07 +0900

    custom hook

--- CustomHook/index.tsx
+++ CustomHook/index.tsx
@@ -2,16 +2,46 @@ import { useState, useEffect } from 'react';
 import React = require('react');
 
 export const CustomHook = () => {
-  const isOnline = useFriendStatus(1);
-  return <div>{`${isOnline}`}</div>;
+  return (
+    <React.Fragment>
+      <RedComponent />
+      <WhiteComponent />
+      <BlueComponent />
+    </React.Fragment>
+  );
 };
 
-function useFriendStatus(friendID) {
-  const [isOnline, setIsOnline] = useState(true);
+const RedComponent = () => {
+  const isPrmaryColors = useCheckPrmaryColors('red');
+  return <div>{`isPrmaryColors：${isPrmaryColors}`}</div>;
+};
+
+const WhiteComponent = () => {
+  const isPrmaryColors = useCheckPrmaryColors('white');
+  return <div>{`isPrmaryColors：${isPrmaryColors}`}</div>;
+};
+
+const BlueComponent = () => {
+  const [color, setColor] = useState(null);
+  const isPrmaryColors = useCheckPrmaryColors(color);
+  const handleOnClick = (color: string) => {
+    setColor(color);
+  };
+  return (
+    <React.Fragment>
+      <button onClick={() => handleOnClick('blue')}>blue</button>
+      <div>{`isPrmaryColors：${isPrmaryColors}`}</div>
+    </React.Fragment>
+  );
+};
+
+function useCheckPrmaryColors(color: string) {
+  const ThreePrimaryColors = ['red', 'blue', 'green'];
+  const [isPrmaryColors, setIsPrmaryColors] = useState(null);
 
   useEffect(() => {
-    setIsOnline(false);
+    setIsPrmaryColors(ThreePrimaryColors.includes(color));
   });
 
-  return isOnline;
+  return isPrmaryColors;
 }
